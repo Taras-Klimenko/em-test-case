7bbeabe8e9ba8b0e4f6316e2d15f6cdec31a0687
Taras-Klimenko <tarasklimenko.1989@gmail.com>
2025-02-23 17:56:36 +0300

    docs: create Swagger API documentation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 import requestsRouter from './routes/requests';
+import YAML from 'yamljs';
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/requests', requestsRouter);
 
 app.get('/', (req, res) => {
